5aa2ce1db0a1ac74492c649d6abe29fdbc336985
gulbalasalamov <g.salamov@gmail.com>
2022-05-04 23:04:02 +0300

    The java.util.function.Function<T, R> interface defines an abstract method named apply that takes an object of generic type T as input and returns an object of generic type R

---
+++ part1.fundementals/src/_3_4_functional_interfaces/FunctionExample.java
@@ -0,0 +1,25 @@
+package _3_4_functional_interfaces;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * create a method map to transform a list of Strings into a list
+ * of Integers containing the length of each String
+ */
+public class FunctionExample {
+    static <T, R> List<R> map(List<T> list, Function<T,R> f) {
+        List<R> result = new ArrayList<>();
+        for (T t: list) {
+            result.add(f.apply(t));
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        var stringlist =  Arrays.asList("Hello","Gulbala","Salamov");
+        var integerList = map(stringlist,(String s) -> s.length());
+        integerList.forEach(System.out::println);
+    }
+}
